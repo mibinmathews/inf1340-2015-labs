66f7925962d97075ca1a8e7ae25f641df65fcae0
mibinmathews <mibin.mathews@mail.utoronto.ca>
2015-10-29 11:20:39 -0400

    updated pytest

--- test_lab3.py
+++ test_lab3.py
@@ -22,15 +22,44 @@ def test_months_with_31():
         assert days_in_month(item) == 31
 
 # Write a test function for the months with 30 days
-
+def test_months_with_30():
+    """
+    Test months with 30 days
+    """
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item) == 30
 
 # Write a test function for the months with 28 or 29 days
+
+def test_months_with_28_29():
+    """
+    Test months with 28 or 29 days
+    """
+    for item in MONTHS_WITH_28_or_29:
+        assert days_in_month(item) == "28 or 29"
 
 
 # Write a test function for months that are not capitalized properly
 # Hint: use the lower() string method
 
+def test_caps():
+    """
+    Test caps
+    """
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item.lower()) == 30
+
+
 # Write a test function for unexpected input
 # Hint: use a try/except block to deal with the exception
 # Hint: use data types other than strings as input
+def test_data_type():
+    """
+    Test for data types
+    """
+    try:
+        assert days_in_month("$$$")
+    except ValueError:
+        return True
+
 
